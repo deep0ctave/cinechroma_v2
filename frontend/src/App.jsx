48214952e4f8a5d5682509a7f8d8c7aefd193d94
deep0ctave <130386277+deep0ctave@users.noreply.github.com>
2025-07-10 17:43:17 +0530

    Finilize base component of Home base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="relative w-full min-h-screen bg-modern-grid">
+  <div className="z-10 relative text-white p-10">
+    <h1 className="text-5xl font-bold">CineChroma</h1>
+    <p className="text-gray-400 mt-2">Explore cinema through color.</p>
+  </div>
+</div>
 
-export default App
+    </BrowserRouter>
+  );
+}
--- frontend/src/components/HueGraph.jsx
+++
@@ -1,1 +0,0 @@
-// Placeholder content for HueGraph.jsx
